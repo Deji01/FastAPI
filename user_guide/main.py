3c8285c06362db28b14c165efb8c1e6a3ad66684
Ayodeji Yekeen <ayodejiyou@gmail.com>
2024-01-17 13:30:24 +0000

    Query Parameters

--- user_guide/main.py
+++ user_guide/main.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Union
 
 from fastapi import FastAPI
 
@@ -13,9 +14,9 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World!"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int):
+#     return {"item_id": item_id}
 
 @app.get("/users/me")
 async def read_user_me():
@@ -38,3 +39,27 @@ async def get_model(model_name: ModelName):
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q":q})
+    if not short:
+        item.update({"description": "This is an amazing item that has a long description"})
+    return item
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = { "item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "This is an amazig item  that has a long description"})
+    return item
+
+    
