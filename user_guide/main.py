471130dd96dd929411d9d838c043c0d228ed205e
Ayodeji Yekeen <ayodejiyou@gmail.com>
2024-01-17 16:43:49 +0000

    Response Body

--- user_guide/main.py
+++ user_guide/main.py
@@ -2,6 +2,16 @@ from enum import Enum
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from pydantic import BaseModel
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
@@ -12,11 +22,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World!"}
-
-# @app.get("/items/{item_id}")
-# async def read_item(item_id: int):
-#     return {"item_id": item_id}
+    return RedirectResponse(url="/docs")
 
 @app.get("/users/me")
 async def read_user_me():
@@ -40,8 +46,6 @@ async def get_model(model_name: ModelName):
 async def read_file(file_path: str):
     return {"file_path": file_path}
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
-
 @app.get("/items/{item_id}")
 async def read_item(item_id: str, q: str | None = None, short: bool = False):
     item = {"item_id": item_id}
@@ -62,4 +66,18 @@ async def read_user_item(
         item.update({"description": "This is an amazig item  that has a long description"})
     return item
 
-    
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result =  {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
+
