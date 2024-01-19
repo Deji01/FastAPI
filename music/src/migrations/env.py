6be6acebe104341cdf41a8333e03edb727ce4082
Ayodeji Yekeen <ayodejiyou@gmail.com>
2024-01-19 23:58:46 +0000

    setup

--- music/src/migrations/env.py
+++ music/src/migrations/env.py
@@ -1,9 +1,12 @@
+import asyncio
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from sqlmodel import SQLModel
 
 from alembic import context
+from ..models import Song
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +21,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
