66ee1edad9d43fb9895826b023e8a7be87b7ab89
Ayodeji Yekeen <ayodejiyou@gmail.com>
2024-01-20 11:56:45 +0000

    setup postgres

--- fastapi-crud/src/app/main.py
+++ fastapi-crud/src/app/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+
 from .api import ping, root
+from .db import engine, database, metadata
+
+metadata.create_all(engine)
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+    
 app.include_router(root.router)
 app.include_router(ping.router)
     
